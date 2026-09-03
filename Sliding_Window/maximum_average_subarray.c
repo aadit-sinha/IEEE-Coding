8200756bb957671366ba6a18d547bab50664de7e
aadit-sinha <aaditstudy29@gmail.com>
2026-09-03 18:53:18 +0530

    Added Maximum Average Subarray I

---
+++ Sliding_Window/maximum_average_subarray.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+double findMaxAverage(int nums[], int n, int k) {
+    int sum = 0;
+
+    for (int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    int maxSum = sum;
+
+    for (int i = k; i < n; i++) {
+        sum = sum - nums[i - k] + nums[i];
+
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+
+    return (double)maxSum / k;
+}
+
+int main() {
+    int nums[] = {1, 12, -5, -6, 50, 3};
+    int n = 6;
+    int k = 4;
+
+    double result = findMaxAverage(nums, n, k);
+
+    printf("Maximum Average = %.5f\n", result);
+
+    return 0;
+}
